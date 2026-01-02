584e3f1a495c498aec173c29fe50f19001f67a58
qazuor <qazuor@gmail.com>
2026-01-02 17:16:56 -0300

    feat(hono): add middleware barrel export

---
+++ packages/hono/src/middleware/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Middleware exports
+ */
+export { createQZPayMiddleware, getQZPay } from './qzpay.middleware.js';
+export { createWebhookMiddleware, createWebhookResponse, getWebhookEvent } from './webhook.middleware.js';
