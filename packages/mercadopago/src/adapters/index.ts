2a8875ac03bf8a296dd3161e09e98289261f33c8
qazuor <qazuor@gmail.com>
2026-01-02 18:59:29 -0300

    feat(mercadopago): add adapters index exports

---
+++ packages/mercadopago/src/adapters/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Adapter exports
+ */
+export { QZPayMercadoPagoCustomerAdapter } from './customer.adapter.js';
+export { QZPayMercadoPagoSubscriptionAdapter } from './subscription.adapter.js';
+export { QZPayMercadoPagoPaymentAdapter } from './payment.adapter.js';
+export { QZPayMercadoPagoCheckoutAdapter } from './checkout.adapter.js';
+export { QZPayMercadoPagoPriceAdapter } from './price.adapter.js';
+export { QZPayMercadoPagoWebhookAdapter } from './webhook.adapter.js';
