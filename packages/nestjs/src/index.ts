f15ce7a14412a7e433d23581324afbbfe1b44403
qazuor <qazuor@gmail.com>
2026-01-03 11:41:47 -0300

    feat(nestjs): add main package exports
    Export all public APIs: module, services, decorators, guards,
    constants, and types for @qazuor/qzpay-nestjs package.

---
+++ packages/nestjs/src/index.ts
@@ -0,0 +1,39 @@
+/**
+ * @qazuor/qzpay-nestjs
+ *
+ * NestJS integration for QZPay billing library
+ */
+
+// Module
+export { QZPayModule } from './qzpay.module.js';
+
+// Services
+export { QZPayService } from './qzpay.service.js';
+export { QZPayWebhookService } from './qzpay-webhook.service.js';
+
+// Decorators
+export { InjectQZPay, RequireEntitlement, RequireSubscription, RateLimit } from './decorators/index.js';
+export type { RequireSubscriptionOptions } from './decorators/index.js';
+
+// Guards
+export { EntitlementGuard, SubscriptionGuard, RateLimitGuard } from './guards/index.js';
+
+// Constants
+export {
+    QZPAY_BILLING_TOKEN,
+    QZPAY_OPTIONS_TOKEN,
+    REQUIRED_ENTITLEMENT_KEY,
+    REQUIRED_SUBSCRIPTION_KEY,
+    RATE_LIMIT_KEY
+} from './constants.js';
+
+// Types
+export type {
+    QZPayModuleOptions,
+    QZPayModuleAsyncOptions,
+    QZPayOptionsFactory,
+    QZPayOptionsFactoryInterface,
+    RateLimitConfig,
+    WebhookEventHandler,
+    WebhookHandlersMap
+} from './types.js';
